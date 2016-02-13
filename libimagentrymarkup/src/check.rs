577141fd818a683a42e34966cb82306cf1a10b97
Matthias Beyer <mail@beyermatthias.de>
2016-02-13 14:59:36 +0100

    Add check interface

---
+++ libimagentrymarkup/src/check.rs
@@ -0,0 +1,34 @@
+use libimagstore::store::Entry;
+
+pub fn is_parsable(e: &Entry) -> bool {
+    unimplemented!()
+}
+
+pub fn is_markdown(e: &Entry) -> bool {
+    false
+}
+
+pub fn is_commonmark(e: &Entry) -> bool {
+    false
+}
+
+pub fn is_textile(e: &Entry) -> bool {
+    false
+}
+
+pub fn is_latex(e: &Entry) -> bool {
+    false
+}
+
+pub fn is_restructuredtext(e: &Entry) -> bool {
+    false
+}
+
+pub fn is_asciidoc(e: &Entry) -> bool {
+    false
+}
+
+pub fn is_bbcode(e: &Entry) -> bool {
+    false
+}
+
