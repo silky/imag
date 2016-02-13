b728330cf554998fe1d1d94bdbe9aaece20a6925
Matthias Beyer <mail@beyermatthias.de>
2016-02-13 15:36:47 +0100

    Implement check::is_parsable() with crossbeam scoped threads

--- libimagentrymarkup/src/check.rs
+++ libimagentrymarkup/src/check.rs
@@ -1,7 +1,25 @@
+use crossbeam;
+
 use libimagstore::store::Entry;
 
 pub fn is_parsable(e: &Entry) -> bool {
-    unimplemented!()
+    crossbeam::scope(|sc| {
+        let is_markdown         = sc.spawn(|| is_markdown(e));
+        let is_commonmark       = sc.spawn(|| is_commonmark(e));
+        let is_textile          = sc.spawn(|| is_textile(e));
+        let is_latex            = sc.spawn(|| is_latex(e));
+        let is_restructuredtext = sc.spawn(|| is_restructuredtext(e));
+        let is_asciidoc         = sc.spawn(|| is_asciidoc(e));
+        let is_bbcode           = sc.spawn(|| is_bbcode(e));
+
+        is_markdown.join()         ||
+        is_commonmark.join()       ||
+        is_textile.join()          ||
+        is_latex.join()            ||
+        is_restructuredtext.join() ||
+        is_asciidoc.join()         ||
+        is_bbcode.join()
+    })
 }
 
 pub fn is_markdown(e: &Entry) -> bool {
