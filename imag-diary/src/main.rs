a88eaa0121c74329c87044a5d83d07b9830e7791
Matthias Beyer <mail@beyermatthias.de>
2016-03-31 18:59:52 +0200

    Start implementing

--- imag-diary/src/main.rs
+++ imag-diary/src/main.rs
@@ -5,6 +5,8 @@ extern crate libimagrt;
 extern crate libimagdiary;
 extern crate libimagutil;
 
+use std::process::exit;
+
 mod ui;
 
 use ui::build_ui;
@@ -28,4 +30,68 @@ fn main() {
     debug!("Hello. Logging was just enabled");
     debug!("I already set up the Runtime object and build the commandline interface parser.");
     debug!("Lets get rollin' ...");
+
+    rt.cli()
+        .subcommand_name()
+        .map(|name| {
+            debug!("Call {}", name);
+            match name {
+                "create" => create(&rt),
+                // "delete" => delete(&rt),
+                // "edit" => edit(&rt),
+                "list" => list(&rt),
+                "diary" => diary(&rt),
+                _        => {
+                    debug!("Unknown command"); // More error handling
+                },
+            }
+        })
+}
+
+fn create(rt: &Runtime) {
+    let diaryname = get_diary_name(rt);
+    if diaryname.is_none() {
+        warn!("No diary selected. Use either the configuration file or the commandline option");
+        exit(1);
+    }
+    let diaryname = diaryname.unwrap();
+
+    let prevent_edit = rt.cli().subcommand_matches("create").unwrap().is_present("no-edit");
+
+    Diary::retrieve(diaryname)
+        .and_then(|diary| diary.new_entry(rt))
+        .and_then(|entry| {
+            if prevent_edit {
+                debug!("Not editing new diary entry");
+            } else {
+                debug!("Editing new diary entry");
+                entry.edit();
+            }
+        })
+
+    unimplemented!()
+}
+
+fn delete(rt: &Runtime) {
+    unimplemented!()
+}
+
+fn edit(rt: &Runtime) {
+    unimplemented!()
+}
+
+fn list(rt: &Runtime) {
+    unimplemented!()
+}
+
+fn diary(rt: &Runtime) {
+    unimplemented!()
+}
+
+
+fn get_diary_name(rt: &Runtime) -> Option<String> {
+    use libimagdiary::config::get_default_diary_name;
+
+    get_default_diary_name(rt)
+        .or(rt.cli().value_of("diaryname").map(String::from))
 }
---
+++ imag-diary/src/ui.rs
@@ -0,0 +1,82 @@
+use clap::{Arg, ArgGroup, App, SubCommand};
+
+pub fn build_ui<'a>(app: App<'a, 'a>) -> App<'a, 'a> {
+    app
+       .arg(Arg::with_name("diaryname")
+            .long("diary")
+            .short("d")
+            .takes_value(true)
+            .required(false)
+            .help("Use other than default diary"))
+
+        .subcommand(SubCommand::with_name("create")
+                   .about("Create a diary entry")
+                   .version("0.1")
+                   .arg(Arg::with_name("no-edit")
+                        .long("no-edit")
+                        .short("e")
+                        .takes_value(false)
+                        .required(false)
+                        .help("Do not edit after creating"))
+                   )
+
+        // TODO: Support deleting diary entries
+        // .subcommand(SubCommand::with_name("delete")
+        //            .about("Delete a diary entry")
+        //            .version("0.1")
+
+        // TODO: Support editing diary entries
+        // .subcommand(SubCommand::with_name("edit")
+        //            .about("Edit a diary entry")
+        //            .version("0.1")
+
+        .subcommand(SubCommand::with_name("list")
+                   .about("List diary entries")
+                   .version("0.1"))
+
+        .subcommand(SubCommand::with_name("diary")
+                   .about("Diary commands")
+                   .version("0.1")
+                    .subcommand(SubCommand::with_name("create")
+                               .about("Create a diary")
+                               .version("0.1")
+                               .arg(Arg::with_name("name")
+                                    .long("name")
+                                    .short("n")
+                                    .takes_value(true)
+                                    .required(true)
+                                    .help("Create Diary with this name"))
+                               .arg(Arg::with_name("no-edit")
+                                    .long("no-edit")
+                                    .short("e")
+                                    .takes_value(false)
+                                    .required(false)
+                                    .help("Do not edit diary comment after creating"))
+                               )
+
+                    .subcommand(SubCommand::with_name("delete")
+                               .about("Delete a diary")
+                               .version("0.1")
+                               .arg(Arg::with_name("name")
+                                    .long("name")
+                                    .short("n")
+                                    .takes_value(true)
+                                    .required(true)
+                                    .help("Delete Diary and all its entries with this name")))
+
+                    .subcommand(SubCommand::with_name("edit")
+                               .about("Edit a diary")
+                               .version("0.1")
+                               .arg(Arg::with_name("name")
+                                    .long("name")
+                                    .short("n")
+                                    .takes_value(true)
+                                    .required(true)
+                                    .help("Edit Diary description of diary with this name")))
+
+                    .subcommand(SubCommand::with_name("list")
+                               .about("List diaries")
+                               .version("0.1"))
+        )
+}
+
