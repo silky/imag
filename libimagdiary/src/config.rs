47ac2d9c1305d1d45f7941ca7c01209e9e6bbf15
Matthias Beyer <mail@beyermatthias.de>
2016-03-31 14:53:35 +0200

    Implement config parser helper functions

--- libimagdiary/src/config.rs
+++ libimagdiary/src/config.rs
@@ -6,18 +6,16 @@ use libimagrt::runtime::Runtime;
 
 pub enum DiaryType {
     Monthly,
-    Weekly,
     Daily,
     Hourly,
     Minutely,
 }
 
-impl From<&str> for DiaryType {
+impl<'a> From<&'a str> for DiaryType {
 
     fn from(s: &str) -> DiaryType {
         match s {
             "monthly"   => DiaryType::Monthly,
-            "weekly"    => DiaryType::Weekly,
             "hourly"    => DiaryType::Hourly,
             "minutely"  => DiaryType::Minutely,
             "daily" | _ => DiaryType::Daily,
@@ -25,30 +23,101 @@ impl From<&str> for DiaryType {
     }
 }
 
-pub fn get_diary_type(rt: &Runtime) -> DiaryType {
-    rt.config()
-        .map(|config| {
-            match config.deref() {
-                &Value::Table(ref t) => {
-                    t.get("diary")
-                        .map(|section| {
-                            match section {
-                                &Value::Table(ref t) => t.get("type").map(DiaryType::from),
-                                _ => {
-                                    debug!("Config error, expected 'diary' to have a table");
-                                    debug!("Falling back to daily entries");
-                                    DiaryType::Daily
-                                },
-                            }
-                        })
-                },
+/// Get the diary type from the configuration.
+///
+/// The configuration must look like this:
+///
+/// ```yaml
+/// [diary]
+/// default = "my diary"
+///
+/// [[diary.diaries]]
+/// name = "my diary"
+/// type = "daily"
+///
+/// [[diary.diaries]]
+/// name = "my other diary"
+/// type = "weekly"
+/// ```
+///
+/// So we have two diaries here, the first one named "my diary" and beeing a daily diary, which says
+/// that entries are created on a per-day basis. The second diary is a weekly diary named "my other
+/// diary". The former diary is the default diary.
+pub fn get_diary_type(rt: &Runtime, diaryname: &String) -> DiaryType {
+    #[inline]
+    fn fallback(s: &str) -> DiaryType {
+        debug!("{}", s);
+        debug!("Falling back to daily entries");
+        DiaryType::Daily
+    }
 
-                _ => {
-                    debug!("Config error, expected config to be a table");
-                    debug!("Falling back to daily entries");
-                    DiaryType::Daily
-                }
+    extract_diary_settings(rt)
+        .map(|section| {
+            match section {
+                &Value::Table(ref t) => t
+                    .get("diaries")
+                    .map(|v| {
+                        if let Some(diary_types) = get_diary_types(v) {
+                            for (name, d_type) in diary_types {
+                                if &name == diaryname {
+                                    return d_type;
+                                }
+                            }
+                        } // no else, as we return in the iteration or we do the following...
+
+                        info!("Could not find diary type settings for {}", diaryname);
+                        fallback("")
+                    })
+                    .unwrap_or_else(|| fallback("Config error, expected 'diary' to have an Array 'diaries'")),
+
+                _ => fallback("Config error, expected 'diary' to have a table")
             }
         })
-        .unwrap_or(DiaryType::Daily)
+        .unwrap_or_else(|| fallback("Config error, expected config to be a table"))
+}
+
+type DiaryName = String;
+
+fn extract_diary_settings<'a>(rt: &'a Runtime) -> Option<&'a Value> {
+    rt.config()
+        .and_then(|config| {
+            match config.deref() {
+                &Value::Table(ref t) => t.get("diary"),
+                _ => None,
+            }
+        })
+}
+
+// Expects a Value::Array which is the "diary.diaries" array
+fn get_diary_types(v: &Value) -> Option<Vec<(DiaryName, DiaryType)>> {
+    match v {
+        &Value::Array(ref a) => {
+            let mut v = vec![];
+            for elem in a {
+                if let Some((name, dtype)) = get_diary_name_and_type_from_tab(elem) {
+                    v.push((name, DiaryType::from(dtype)));
+                }
+            }
+            Some(v)
+        },
+        _ => None,
+    }
+}
+
+fn get_diary_name_and_type_from_tab(v: &Value) -> Option<(DiaryName, DiaryType)> {
+    match v {
+        &Value::Table(ref t) => {
+            if let Some(&Value::String(ref name)) = t.get("name") {
+                if let Some(&Value::String(ref dtype)) = t.get("type") {
+                    return Some((name.clone(), DiaryType::from(&dtype[..])));
+                }
+            }
+            None
+        }
+        _ => None,
+    }
+}
+
+pub fn get_default_diary_name(rt: &Runtime) -> Option<String> {
+    unimplemented!()
 }
