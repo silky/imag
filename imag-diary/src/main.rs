1b1cccdf50b9d29d560d2ac02243e2f9a71f2d05
Matthias Beyer <mail@beyermatthias.de>
2016-03-31 15:40:24 +0200

    Add dependencies

--- imag-diary/src/main.rs
+++ imag-diary/src/main.rs
@@ -1,3 +1,10 @@
+#[macro_use] extern crate log;
+extern crate clap;
+
+extern crate libimagrt;
+extern crate libimagdiary;
+extern crate libimagutil;
+
 fn main() {
     println!("Hello, world!");
 }
