32264e2470fb4f509874827fc1aa1a51e2aaad77
Matthias Beyer <mail@beyermatthias.de>
2016-03-31 15:46:53 +0200

    Add initial main() code

--- imag-diary/src/main.rs
+++ imag-diary/src/main.rs
@@ -10,5 +10,22 @@ mod ui;
 use ui::build_ui;
 
 fn main() {
-    println!("Hello, world!");
+    let name = "imag-diary";
+    let version = &version!()[..];
+    let about = "Personal Diary/Diaries";
+    let ui = build_ui(Runtime::get_default_cli_builder(name, version, about));
+    let rt = {
+        let rt = Runtime::new(ui);
+        if rt.is_ok() {
+            rt.unwrap()
+        } else {
+            println!("Could not set up Runtime");
+            println!("{:?}", rt.err().unwrap());
+            exit(1);
+        }
+    };
+
+    debug!("Hello. Logging was just enabled");
+    debug!("I already set up the Runtime object and build the commandline interface parser.");
+    debug!("Lets get rollin' ...");
 }
