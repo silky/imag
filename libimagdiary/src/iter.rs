a9afebcf145742a4816c74ba11ebb1c5dafb4619
Matthias Beyer <mail@beyermatthias.de>
2016-04-01 16:33:12 +0200

    Impl DiaryMonthIterator::next()

--- libimagdiary/src/iter.rs
+++ libimagdiary/src/iter.rs
@@ -115,7 +115,23 @@ impl<'a> Iterator for DiaryMonthIterator<'a> {
     type Item = Result<DiaryEntry<'a>>;
 
     fn next(&mut self) -> Option<Result<DiaryEntry<'a>>> {
-        unimplemented!()
+        loop {
+            match self.iter.next() {
+                None           => return None,
+                Some(Err(e))   => return Some(Err(e)),
+                Some(Ok(note)) => {
+                    if note.deref()
+                        .get_location()
+                        .to_str()
+                        .and_then(DiaryId::parse)
+                        .map(|id| id.month() == self.month)
+                        .unwrap_or(false)
+                    {
+                        return Some(Ok(note))
+                    }
+                }
+            }
+        }
     }
 
 }
