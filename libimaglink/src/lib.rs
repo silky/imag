1879583a17a3e035ad12ed2d6e0284030ba2622a
Matthias Beyer <mail@beyermatthias.de>
2016-04-09 15:49:36 +0200

    Add module_entry_path_mod!() call, so we have an own module path for the link library

--- libimaglink/src/lib.rs
+++ libimaglink/src/lib.rs
@@ -3,7 +3,9 @@ extern crate toml;
 extern crate url;
 extern crate sodiumoxide;
 
-extern crate libimagstore;
+#[macro_use] extern crate libimagstore;
+
+module_entry_path_mod!("links", "0.1.0");
 
 pub mod error;
 pub mod external;
